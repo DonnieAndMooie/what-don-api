e05b9abd4333a1a76f985b65554f3419fc3ce942
Owen Eaves <owenjeaves@gmail.com>
2023-12-07 13:26:53 +0000

    Edit url

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const playerData = require("../data").playerData
 
 
 router.get("/fetch-player", async function(req, res){
-  res.setHeader("Access-Control-Allow-Origin", "https://donnieandmooie.github.io/what-don/");
+  res.setHeader("Access-Control-Allow-Origin", "https://donnieandmooie.github.io/");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
