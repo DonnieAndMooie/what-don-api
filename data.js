d22445d83bc9a2bc3b96ddbd32d6be9da3aae684
Owen Eaves <owenjeaves@gmail.com>
2024-01-25 14:49:51 +0000

    Change database

--- data.js
+++ data.js
@@ -30179,10 +30179,10 @@ const playerData = [
   ]
 
 
-  for (const player of playerData){
-    const newPlayer = new Player(player)
-    console.log(player.name)
-    newPlayer.save()
-  } 
+  // for (const player of playerData){
+  //   const newPlayer = new Player(player)
+  //   console.log(player.name)
+  //   newPlayer.save()
+  // } 
 
   exports.playerData = playerData
